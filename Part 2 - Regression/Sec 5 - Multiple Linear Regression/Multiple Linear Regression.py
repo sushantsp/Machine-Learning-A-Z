c092bc97c6d31e4a57dcc547900a3792ee06b634
Sushant <32874962+sapiensush@users.noreply.github.com>
2019-11-20 22:15:55 +0530

    Update Multiple Linear Regression.py

--- Part 2 - Regression/Sec 5 - Multiple Linear Regression/Multiple Linear Regression.py
+++ Part 2 - Regression/Sec 5 - Multiple Linear Regression/Multiple Linear Regression.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#iloc Means Integer Location in Pandas index
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
